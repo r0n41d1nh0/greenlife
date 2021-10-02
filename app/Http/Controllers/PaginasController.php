2049cc3b6ee4b5aebae2d468c2a45d7a89bea185
Ronald Gomez <vaibanezronald@gmail.com>
2021-10-02 08:02:50 +0000

    Se adiciona el primer dashboard

--- app/Http/Controllers/PaginasController.php
+++ app/Http/Controllers/PaginasController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\DB;
+use App\Salida;
 
 class PaginasController extends Controller
 {
 
   public function home()
 	{
+		$compras = DB::table('ingreso_detalle')->select(DB::raw('sum(precio*cantidad) total'))->get();		
+		$salidas = Salida::lista()->get();
 
-			return view('paginas.home');
+		return view('paginas.home',compact(['compras','salidas']));
 
 	}
 }
--- resources/views/paginas/home.blade.php
+++ resources/views/paginas/home.blade.php
@@ -7,9 +7,41 @@
     <li class="breadcrumb-item active">Inicio</li>
   </ol>
   <div class="row">
-    <div class="col-12">
-      <h1>Bienvenid@</h1>
-      <p>Control de ventas</p>
+    <div class="col-3">
+      <div class="card border-light text-center text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h2>S/.  {{ $compras[0]->total }}</h2>
+          <p class="card-text">Gastos en compras</p>
+        </div>
+        <div class="card-footer"><a href="{{ route('ingresos.lista') }}" class="link-light"> Mas información</a></div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card border-light text-center text-white bg-success mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h2>S/. {{ $salidas->sum('ganancia') + $salidas->sum('precio_delivery') + $salidas->sum('costo_compra') - $salidas->sum('costo_delivery') - $salidas->sum('costo_agencia') - $salidas->sum('costo_retorno') }}</h2>
+          <p class="card-text">En ganancias</p>
+        </div>
+        <div class="card-footer"><a href="{{ route('salidas.lista') }}" class="link-light"> Mas información</a></div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card border-light text-center text-white bg-warning mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h2>S/. {{ $salidas->sum('venta_total') }}</h2>
+          <p class="card-text">En Ventas</p>
+        </div>
+        <div class="card-footer"><a href="{{ route('salidas.lista') }}" class="link-light"> Mas información</a></div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card border-light text-center text-white bg-danger mb-3" style="max-width: 18rem;">
+      <div class="card-body">
+          <h2>S/. {{ $salidas->sum('costo_delivery') + $salidas->sum('costo_agencia') + $salidas->sum('costo_retorno') }}</h2>
+          <p class="card-text">En Costos</p>
+        </div>
+        <div class="card-footer"><a href="{{ route('salidas.lista') }}" class="link-light"> Mas información</a></div>
+      </div>
     </div>
   </div>
 @endsection
